663d44e5638635d72c92c66127ddaa61869928df
JaminBenjamins <jaminbenjamins@gmail.com>
2023-03-07 11:52:48 -0800

    print formatted string

--- 0x01-python-if_else_loops_functions/12-fizzbuzz.py
+++ 0x01-python-if_else_loops_functions/12-fizzbuzz.py
@@ -7,12 +7,14 @@
     For multiples of three and five, print FizzBuzz instead of the number.
     """
 def fizzbuzz():
-    for i in range (1,101):
+
+
+    for i in range(1, 101):
         if i % 3 == 0:
             print("Fizz", end="")
         elif i % 5 == 0:
             print("Buzz", end="")
         elif i % 3 == 0 and i % 5 == 0:
-            print("FizzBuzz" end="")
+            print("FizzBuzz", end="")
         else:
-            print("{}".format(i),end="")
+            print("{}".format(i), end="")
