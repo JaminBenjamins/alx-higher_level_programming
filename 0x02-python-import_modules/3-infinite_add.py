3845d51db7732821c7405183b4aa1e8127948acf
JaminBenjamins <jaminbenjamins@gmail.com>
2023-03-09 12:38:11 -0800

    Kepp printing

---
+++ 0x02-python-import_modules/3-infinite_add.py
@@ -0,0 +1,9 @@
+#!/usr/bin/python3 
+if __name == "__main__":
+     """ print all arguments """
+     import sys
+
+     count = 0
+     for i in range(len(sys.argv - 1)):
+         count += int(sys.argv[i + 1])
+    print("{}".format(count))
