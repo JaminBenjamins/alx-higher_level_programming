7c942d9b0e21bc419655b89ead351dce7c30f81c
JaminBenjamins <jaminbenjamins@gmail.com>
2023-03-13 23:19:58 -0700

    Element at

--- 0x03-python-data_structures/1-element_at.py
+++ 0x03-python-data_structures/1-element_at.py
@@ -2,6 +2,4 @@
 def element_at(my_list, idx):
     if idx < 0 or idx > len(my_list) - 1:
         return None
-    for i in range(my_list):
-        idx = my_list[i]
-    return idx
+    return (my_list(idx))
