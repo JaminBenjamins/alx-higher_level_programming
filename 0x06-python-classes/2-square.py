6f96af1fdb7cdaef607ad0d05370d79a635055d7
JaminBenjamins <jaminbenjamins@gmail.com>
2023-03-29 03:39:26 -0700

    Defining attributes of an object

--- 0x06-python-classes/2-square.py
+++ 0x06-python-classes/2-square.py
@@ -5,4 +5,4 @@ class Square:
             raise TypeError("sizze must be an integer")
         elif size < 0:
             raise ValueError("size must be >=0")
-       self.__size = size 
+        self.__size = size 
