5214f74a630fdc98220ced3bfe7514845c3a0f2b
JaminBenjamins <jaminbenjamins@gmail.com>
2023-03-29 04:11:03 -0700

    adding objects to the class

--- 0x06-python-classes/3-square.py
+++ 0x06-python-classes/3-square.py
@@ -1,6 +1,15 @@
 #!/usr/bin/python3
+""" Defining a class Square """
+
 class Square:
+    #Instatiating a class
     def __init__(self,size=0):
+        """
+            Initialising a new square
+
+            Args: 
+                size(int): size of a new square
+        """
         if not isinstance(size, int):
             raise TypeError("size must be an integer")
         elif size < 0:
@@ -8,6 +17,7 @@ class Square:
         self.__size = size
 
     def area(self):
+        """ Returns area of square """
         return (self.__size * self.__size)
 
 
