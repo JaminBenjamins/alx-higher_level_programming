2483379276d7554a2ed15618db9ba1ef02c0c60d
JaminBenjamins <jaminbenjamins@gmail.com>
2023-03-29 03:30:45 -0700

    Setting attributes of a square

--- 0x06-python-classes/2-square.py
+++ 0x06-python-classes/2-square.py
@@ -1,7 +1,7 @@
 #!/usr/bin/python3
 class Square:
     def __init__(self,size=0):
-        if size is not isinstance:
+        if not isinstance(size, int):
             raise TypeError("sizze must be an integer")
         elif size < 0:
             raise ValueError("size must be >=0")
