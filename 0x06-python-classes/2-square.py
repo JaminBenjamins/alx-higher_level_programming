a801eb73c08d283655f33964d474d30c60805bfa
JaminBenjamins <jaminbenjamins@gmail.com>
2023-03-29 04:27:28 -0700

    Adding attribute to square

--- 0x06-python-classes/2-square.py
+++ 0x06-python-classes/2-square.py
@@ -1,6 +1,16 @@
 #!/usr/bin/python3
+""" Defining a class """
+
+
 class Square:
+    """ Initializing a class """
+
     def __init__(self,size=0):
+        """
+
+            Args:
+                size(int): Value of square
+        """
         if not isinstance(size, int):
             raise TypeError("sizze must be an integer")
         elif size < 0:
