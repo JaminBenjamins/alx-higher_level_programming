9ec4783ab71de368eb62b80db057fddc51c57bf9
JaminBenjamins <jaminbenjamins@gmail.com>
2023-03-29 04:16:53 -0700

    Initializing a class

--- 0x06-python-classes/1-square.py
+++ 0x06-python-classes/1-square.py
@@ -1,4 +1,12 @@
 #!/usr/bin/python3
+""" Defining a class """
+
+
 class Square:
+    """ Intializing the class """
     def __init__(self,size):
+        """
+            Args:
+                size(int) : size of new square
+        """
         self.__size = size
