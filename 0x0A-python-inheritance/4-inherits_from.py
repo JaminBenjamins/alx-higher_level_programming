cd1e85373c7ce33ce20582a03d65c2c28f7f57c1
JaminBenjamins <jaminbenjamins@gmail.com>
2023-04-11 09:25:13 -0700

    Checking if object is from inheritance

---
+++ 0x0A-python-inheritance/4-inherits_from.py
@@ -0,0 +1,18 @@
+#!/usr/bin/pytthon3
+""" A function that checks if an object belongs to the class it inherited """
+
+
+def inherits_from(obj, a_class):
+    """ Checks if an object is inherited from a class
+
+        Args:
+            obj - the attribute to check if it belongs to the class
+            a_class - the class to check from
+
+        Returns:
+            True if object is a subclass of a_class
+            False Otherwise
+    """
+    if issubclass(type(obj), a_class) and type(obj) != a_class:
+        return True
+    return False
