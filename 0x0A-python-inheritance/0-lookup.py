766d563e1ab6b0f08d3fc2d93671f53eca16c957
JaminBenjamins <jaminbenjamins@gmail.com>
2023-04-11 04:46:55 -0700

    Function that looks up attributes of a class

---
+++ 0x0A-python-inheritance/0-lookup.py
@@ -0,0 +1,7 @@
+#!/usr/bin/python3
+""" Defining a function that returns attribute and methods """
+
+
+def lookup(obj):
+    """ a function that returns objects of a class """
+    return (dir(obj))
