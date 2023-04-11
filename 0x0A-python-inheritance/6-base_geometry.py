45d262d066a06690fe9b399f509474828c6a7e0a
JaminBenjamins <jaminbenjamins@gmail.com>
2023-04-11 09:26:18 -0700

    Raising an exception to an object

---
+++ 0x0A-python-inheritance/6-base_geometry.py
@@ -0,0 +1,10 @@
+#!/usr/bin/python3
+""" Defining a class """
+
+
+class BaseGemoetry:
+    """ Defining attributes of a class """
+    def area(self):
+        """ Raising an exception for area function """
+        raise Exception("area () is not implemented")
+
