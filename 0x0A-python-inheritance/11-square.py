7beb5320c68be42496ce0e7db6a0feab890065c1
JaminBenjamins <jaminbenjamins@gmail.com>
2023-04-11 18:16:35 -0700

    Adding attributes to a square

---
+++ 0x0A-python-inheritance/11-square.py
@@ -0,0 +1,16 @@
+#!/usr/bin/python3
+""" a class that inherits from another and prints """
+Rectangle = __import__('9-rectangle').Rectangle
+
+
+def __init__(self, size):
+    """ Inheriting attribuutes from a class
+        Args:
+            size - the size of square
+
+        Return:
+            The square description
+    """
+    self.integer_validator("width", width)
+    super.__init__(size, size)
+    self.__size = size
