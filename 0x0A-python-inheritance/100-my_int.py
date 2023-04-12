82a50838cc6d0b096fbf90e018bf12399b98f871
JaminBenjamins <jaminbenjamins@gmail.com>
2023-04-12 06:26:58 -0700

    Negating comparison operations

--- 0x0A-python-inheritance/100-my_int.py
+++ 0x0A-python-inheritance/100-my_int.py
@@ -6,7 +6,7 @@ class MyInt(int):
     """ The class has boolean operations inverted """
 
     def __eq__(self, number):
-        self.value != number
+        return self.real != number
 
     def __ne__(self, number):
-        self.value == number
+        return self.real == number
