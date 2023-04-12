dba84ba2ee569f54f98b0ba232466993330e4a3f
JaminBenjamins <jaminbenjamins@gmail.com>
2023-04-12 06:16:23 -0700

    Class inheritance

--- 0x0A-python-inheritance/11-square.py
+++ 0x0A-python-inheritance/11-square.py
@@ -3,14 +3,15 @@
 Rectangle = __import__('9-rectangle').Rectangle
 
 
-def __init__(self, size):
-    """ Inheriting attribuutes from a class
-        Args:
-            size - the size of square
+class Square(Rectangle):
+    def __init__(self, size):
+        """ Inheriting attribuutes from a class
+            Args:
+                size - the size of square
 
-        Return:
-            The square description
-    """
-    self.integer_validator("width", width)
-    super.__init__(size, size)
-    self.__size = size
+            Return:
+                The square description
+        """
+        self.integer_validator("size", size)
+        super().__init__(size, size)
+        self.__size = size
