1bf901c918d62729bba23f51961be67b436c7e5a
JaminBenjamins <jaminbenjamins@gmail.com>
2023-04-12 05:57:19 -0700

    Creating a subclass inheriting properties

--- 0x0A-python-inheritance/10-square.py
+++ 0x0A-python-inheritance/10-square.py
@@ -2,11 +2,14 @@
 """ Defines a square a subclass of Rectangle """
 Square = __import__('9-rectangle').Square
 
-def __init__(self, size):
-    """ Initializing the class 
-        Args: 
-            size - the size of a square
-    """
-    super.integer_validator("width", width)
-    super().__init__(size,size)
-    self.__size
+
+class Square(Rectangle):
+    """ Defining a child class """
+    def __init__(self, size):
+        """ Initializing the class 
+            Args: 
+                size - the size of a square
+        """
+        super.integer_validator("width", width)
+        super().__init__(size,size)
+        self.__size
