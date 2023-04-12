4f4ad55ab9df35752e8a85b072bde9ba89f067a1
JaminBenjamins <jaminbenjamins@gmail.com>
2023-04-12 16:50:46 -0700

    A function that appends aa tring to txt file

---
+++ 0x0B-python-input_output/2-append_write.py
@@ -0,0 +1,15 @@
+#!/usr/bin/python3
+""" a function that append a file """
+
+
+def append_write(filename="", text=""):
+    """ Apend a string tp UTF-8 file
+        Args:
+            filename = the file where text is appended 
+            text - the text to be appended
+
+        Return:
+            Number of characters added
+    """
+    with open(filename, "a", unicode="utf-8") as f:
+        return f.write(text)
