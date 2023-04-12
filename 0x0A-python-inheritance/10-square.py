29d5bee91282bf4728bef534a4ef00cfd89d7fb5
JaminBenjamins <jaminbenjamins@gmail.com>
2023-04-11 17:30:08 -0700

    Uing inherited attribute in a class

---
+++ 0x0A-python-inheritance/10-square.py
@@ -0,0 +1,12 @@
+#!/usr/bin/python3
+""" Defines a square a subclass of Rectangle """
+Square = __import__('9-rectangle').Square
+
+def __init__(self, size):
+    """ Initializing the class 
+        Args: 
+            size - the size of a square
+    """
+    super.integer_validator("width", width)
+    super().__init__(size,size)
+    self.__size
