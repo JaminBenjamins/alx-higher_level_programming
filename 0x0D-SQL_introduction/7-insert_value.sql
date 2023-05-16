b72839432342c53ba2ed49f3d95692609b430dc6
JaminBenjamins <jaminbenjamins@gmail.com>
2023-05-17 01:08:04 +0300

    A script that inserts a value to table

--- 0x0D-SQL_introduction/7-insert_value.sql
+++ 0x0D-SQL_introduction/7-insert_value.sql
@@ -1,3 +1,2 @@
 -- A script that inserts a value in a database
-INSERT INT0 `first_table` (`id`, `name`)
-VALUES (89, "Best School");
+INSERT INT0 first_table (`id`, `name`) VALUES (89, "Best School");
