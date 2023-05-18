749c3b54b34d191ba7c08ae7439c2fbf3e5c5ea2
JaminBenjamins <jaminbenjamins@gmail.com>
2023-05-18 22:01:33 +0300

    A script that prints the average temperatures of a a city

--- 0x0D-SQL_introduction/102-top_city.sql
+++ 0x0D-SQL_introduction/102-top_city.sql
@@ -1,6 +1,6 @@
 -- A scripit that displays the maximum temperature 
 -- in each city in July and August in a database
-SELECT city AVG(temp) as max_temp
+SELECT city AVG(temp) AS max_temp
 FROM temperatures 
 WHERE month = 7 OR month = 8
 GROUP BY city
