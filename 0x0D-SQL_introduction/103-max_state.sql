ce0e401e67e2691cbd41141e33481621375b3d8e
JaminBenjamins <jaminbenjamins@gmail.com>
2023-05-18 22:34:23 +0300

    Adding backticks

--- 0x0D-SQL_introduction/103-max_state.sql
+++ 0x0D-SQL_introduction/103-max_state.sql
@@ -1,5 +1,5 @@
 -- A script that displays maximum temperature of each state
-SELECT city MAX(value) as max_val
-FROM temperatures
-GROUP BY state
-ORDER BY state;
+SELECT city MAX(`value`) AS `max_val`
+FROM `temperatures`
+GROUP BY `state`
+ORDER BY `state`;
