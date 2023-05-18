aefcbe0621e8ea6e6f50bbaff01660a9ae113ac0
JaminBenjamins <jaminbenjamins@gmail.com>
2023-05-18 22:38:09 +0300

    Appropriating variable type

--- 0x0E-SQL_more_queries/10-genre_id_by_show.sql
+++ 0x0E-SQL_more_queries/10-genre_id_by_show.sql
@@ -2,5 +2,5 @@
 SELECT tv_shows.title, tv_show_genres.genre_id
 FROM tv_show_genres
 INNER JOIN tv_shows
-ON tv_shows.id tv_show_genres.show_id
+ON tv_shows.id tv_show_genres.genre_id
 ORDER BY tv_shows.title, tv_show_genres.genre_id;
