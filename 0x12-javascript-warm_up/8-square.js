ee34260686637e14f3f0317add3c5eeddcd9bec3
jaminbenjamins <jaminbenjamins@gmail.com>
2023-06-14 22:28:17 +0300

    Refactoring code

--- 0x12-javascript-warm_up/8-square.js
+++ 0x12-javascript-warm_up/8-square.js
@@ -1,11 +1,11 @@
 #!/usr/bin/node
-const { argv } = require('process');
-const size = Number(argv[2]);
-const repeat = 'x'.repeat(size);
-if (isNan(size)) {
-	console.log('Missing size');
+const size = Math.floor(Number(process.argv[2]));
+if (isNaN(size)) {
+  console.log('Missing size');
 } else {
-	for (let i = 0; i < size; i++) {
-		console.log(repeat);
-	}
+  for (let r = 0; r < size; r++) {
+    let row = '';
+    for (let c = 0; c < size; c++) row += 'X';
+    console.log(row);
+  }
 }
