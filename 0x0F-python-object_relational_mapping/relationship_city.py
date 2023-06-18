052b01b7c12e8267e144ef09f2917f0603766657
jaminbenjamins <jaminbenjamins@gmail.com>
2023-06-18 23:00:53 +0300

    Define attributes of city  instance

---
+++ 0x0F-python-object_relational_mapping/relationship_city.py
@@ -0,0 +1,24 @@
+#!/usr/bin/python3
+"""
+    Defines a city class that inherits from base
+    of the sqlalchemy linka to table cities
+"""
+from sqlalchemy import Column, ForeignKey, Integer, String
+from sqlalchemy.ext.declarative import declarative_base
+
+Base = declarative_base()
+
+
+class City(Base):
+    """
+        Represent a city from mysql database
+
+    Attributes:
+            id(str): The id of the city
+            name(string): Name of the city
+            state_id(integer): City's state id
+    """
+    __table__name = "cities"
+    id = Column(Integer, primary_key=True)
+    name = Column(String(128), nullable=False)
+    state_id = Column(Integer, ForeignKey("states.id"), nullable=False)
