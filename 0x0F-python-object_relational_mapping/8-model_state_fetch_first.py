f418406ffd1f5b7c4847359899c27369d703cd3f
JaminBenjamins <jaminbenjamins@gmail.com>
2023-06-23 18:09:12 +0300

    Sorting output regex pattern

--- 0x0F-python-object_relational_mapping/8-model_state_fetch_first.py
+++ 0x0F-python-object_relational_mapping/8-model_state_fetch_first.py
@@ -23,4 +23,4 @@ if __name__ == "__main__":
     if state is None:
         print("Nothing")
     else:
-        print("{}.{}".format(state.id, state.name))
+        print("{}: {}".format(state.id, state.name))
