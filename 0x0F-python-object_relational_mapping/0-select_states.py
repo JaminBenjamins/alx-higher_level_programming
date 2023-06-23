8fd1f5925c355e770b3da09173763ef3859b46d4
JaminBenjamins <jaminbenjamins@gmail.com>
2023-06-23 07:27:39 +0300

    Documenting module appropriately

--- 0x0F-python-object_relational_mapping/0-select_states.py
+++ 0x0F-python-object_relational_mapping/0-select_states.py
@@ -1,6 +1,6 @@
 #!/usr/bin/python3
 """
-    List all states from the databaase 
+    List all states from the databaase
     Usage: ./0-select states.py <mysql username>\
                                 <mysql password> \
                                 <database name>
@@ -8,7 +8,7 @@
 import sys
 import MySQLdb
 
-if __name__ = "__main__":
+if __name__ == "__main__":
     db = MySQLdb.connect(user=sys.argv[1], passwd=sys.argv[2], db=sys.argv[3])
     c = db.cursor()
     c.execute("SELECT * FROM `states`")
