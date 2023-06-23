d4811dd380b629b33376fef168f86b486c185eac
JaminBenjamins <jaminbenjamins@gmail.com>
2023-06-23 12:06:52 +0300

    List a state on given name

--- 0x0F-python-object_relational_mapping/2-my_filter_states.py
+++ 0x0F-python-object_relational_mapping/2-my_filter_states.py
@@ -1,7 +1,7 @@
 #!/usr/bin/python3
 """
     Takes an argument and displays all values
-    in the states table of a database hbtn_0e_0_usa 
+    in the states table of a database hbtn_0e_0_usa
     where name matches the argument
     Usage: ./2-my_filter_states.py <mysql username>
                                    <mysql password>
@@ -11,10 +11,10 @@
 import sys
 import MySQLdb
 
-if __name__ = "__main__":
+if __name__ == "__main__":
     db = MySQLdb.connect(user=sys.argv[1], passwd=sys.argv[2], db=sys.argv[3])
     c = db.cursor()
     c.execute("""SELECT * FROM statess
                 WHERE name LIKE BINARY '{}'
                 ORDER BY states.id ASC""".format(sys.argv[4]).strip("'"))
-    [print(state) for state in c.fetchall()]
+    [print(state) for state in c.fetchall()
