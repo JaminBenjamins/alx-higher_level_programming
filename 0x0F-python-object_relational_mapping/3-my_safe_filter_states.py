d03cd587891ec6d8c557f6fe9e56927d5df11910
JaminBenjamins <jaminbenjamins@gmail.com>
2023-06-23 12:19:41 +0300

    Retrieve a record free from injections

--- 0x0F-python-object_relational_mapping/3-my_safe_filter_states.py
+++ 0x0F-python-object_relational_mapping/3-my_safe_filter_states.py
@@ -1,7 +1,7 @@
 #!/usr/bin/python3
 """
     Display all values in the states table of the database hbtn_0e_0_usa
-    whose nam matches supplied argument. 
+    whose nam matches supplied argument.
     Safe from MySQL injections.
     Usage: ./3-my_safe_filter_states.py <mysql username> \
                                         <mysql password> \
@@ -11,7 +11,7 @@
 import sys
 import MySQLdb
 
-if __name__ = "__main__":
+if __name__ == "__main__":
     db = MySQLdb.connect(user=sys.argv[1], passwd=sys.argv[2], db=sys.argv[3])
     c = db.cursor()
     c.execute("SELECT * FROM `states`")
