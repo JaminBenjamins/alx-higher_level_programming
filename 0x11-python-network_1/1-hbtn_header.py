731f78f4b7f9edf41e9c0a816916867df97a078d
JaminBenjamins <jaminbenjamins@gmail.com>
2023-07-02 20:27:23 +0300

    Find a variable in header and print it

---
+++ 0x11-python-network_1/1-hbtn_header.py
@@ -0,0 +1,12 @@
+"""Takes a URL sends a request to the URL and fetches
+the X-Request-Id parameter found in header of response
+"""
+
+import sys
+import urllib
+
+if __name__ == "__main__":
+    url = sys.argv[1]
+    request = urllib.request.Request(url)
+    with urllib.request.urlopen(request) as response:
+        print(dict(response.headers.get("X-Request-Id"))
