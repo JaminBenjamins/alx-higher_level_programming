3d2db3c8c78c9ada201afa1a508007719c0ad559
JaminBenjamins <jaminbenjamins@gmail.com>
2023-07-03 19:02:37 +0300

    Adding proper string holding parameter

--- 0x11-python-network_1/2-post_email.py
+++ 0x11-python-network_1/2-post_email.py
@@ -7,7 +7,7 @@ import urllib.request
 
 if __name__ == "__main__":
     url = sys.argv[1]
-    value = {"email": sys.argv[2]}
+    value = {"Your email is": sys.argv[2]}
     data = urllib.parse.urlencode(value).encode("ascii")
 
     request = urllib.request.Request(url, data)
