8567a972a0b5e6c91b86145db5216f02e0e6fbb6
JaminBenjamins <jaminbenjamins@gmail.com>
2023-07-03 19:28:11 +0300

    Print body response of http request handling http errors

---
+++ 0x11-python-network_1/3-error_code.py
@@ -0,0 +1,20 @@
+#!/usr/bin/python3
+"""
+    Takes an URL and sends request and display
+    back the body of response handling exceptions
+"""
+
+import sys
+import urllib.error
+import urllib.request
+
+
+if __name__ == "__main__":
+    url = sys.argv[1]
+
+    request = urllib.request.Request(url)
+    try:
+        with urllib.request.urlopen(request) as response:
+            print(response.read().decode("ascii"))
+    except urllib.error.HTTPError as e:
+        print("Error code: {}".format(e.code))
