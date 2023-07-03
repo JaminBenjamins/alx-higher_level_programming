2f9aa5eea63b9a842eaddee6a04543506cd01b8c
JaminBenjamins <jaminbenjamins@gmail.com>
2023-07-03 19:43:07 +0300

    Display the response of a url in specified format

---
+++ 0x11-python-network_1/4-hbtn_status.py
@@ -0,0 +1,12 @@
+#!/usr/bin/python3
+"""
+    Fetches a URL and display result in specific format
+"""
+import requests
+
+
+if __name__ == "__main__":
+    r = requests.get("https://alx-intranet.hbtn.io/status")
+    print("Body response:")
+    print("\t- type: {}".format(type(r.text)))
+    print("\t-content: {}".format(r.text))
