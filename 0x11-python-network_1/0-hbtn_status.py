a45ef25ea726a3609a8a41265440c3a1e5392524
JaminBenjamins <jaminbenjamins@gmail.com>
2023-07-03 19:09:09 +0300

    A script that retrieves a response from a url

--- 0x11-python-network_1/0-hbtn_status.py
+++ 0x11-python-network_1/0-hbtn_status.py
@@ -8,6 +8,6 @@ if __name__ == "__main__":
     with request.urlopen("https://alx-intranet.hbtn.io/status") as response:
         response = response.read()
         print("Body response:")
-        print("\t- type: {}".format(type(responses)))
+        print("\t- type: {}".format(type(response)))
         print("\t- content: {}".format(response))
         print("\t- utf8 content: {}".format(response.decode(encoding='utf-8')))
